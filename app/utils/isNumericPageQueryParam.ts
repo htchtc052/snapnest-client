293801b0690abc69ed13c8d7f0e31745484b56d8
alonecat <htchtc052@gmail.com>
2026-05-11 22:38:26 +0300

    remove unused utility helpers

--- app/utils/isNumericPageQueryParam.ts
+++
@@ -1,3 +0,0 @@
-export function isNumericPageQueryParam(value: unknown): boolean {
-  return value === undefined || (typeof value === 'string' && /^[1-9]\d*$/.test(value))
-}
--- app/utils/isNumericParam.ts
+++
@@ -1,3 +0,0 @@
-export function isNumericParam(value: unknown): value is string {
-  return typeof value === 'string' && /^\d+$/.test(value)
-}
