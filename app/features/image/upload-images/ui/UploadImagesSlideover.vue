3ab6838399eb14fcf92cdda1cd6133b5c8489764
alonecat <htchtc052@gmail.com>
2026-05-18 21:26:40 +0300

    refactor: remove custom upload drawer sizing

--- app/features/image/upload-images/ui/UploadImagesSlideover.vue
+++ app/features/image/upload-images/ui/UploadImagesSlideover.vue
@@ -12,12 +12,6 @@ const emit = defineEmits<{
     :dismissible="false"
     :handle="false"
     inset
-    :ui="{
-      content: 'left-auto right-4 h-36 w-[min(34rem,calc(100%-2rem))] max-h-[calc(100%-2rem)]',
-      container: 'gap-0 p-0',
-      header: 'flex h-20 items-center justify-between border-b border-default px-6',
-      body: 'min-h-0 flex-1',
-    }"
   >
     <template #header>
       <h2 class="text-lg font-semibold text-highlighted">
